cbe37bd1441d528cfef0929e16baa6dc511e498a
Yassin Khaled <yk7818126@gmail.com>
2025-11-23 01:05:38 +0200

    last one inshalla email send

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -9,19 +9,18 @@ export const ContactSection = () => {
   const {toast} = useToast()
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+ const handleSubmit = () => {
+  setIsSubmitting(true)
 
-    setIsSubmitting(true)
-
-    setTimeout (() => {
-      toast({
-        title: "Message sent!",
-        description: "Thank you for your message. I'll get back to you soon."
-      })
+  setTimeout(() => {
+    toast({
+      title: "Message sent!",
+      description: "Thank you for your message. I'll get back to you soon."
+    })
     setIsSubmitting(false)
-    }, 1500)
-  }
+  }, 1500)
+}
+
   return <section id="contact" className="py-24 px-4 relative bg-secondaty/30">
     <div className="container mx-auto max-w-5xl">
     <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Get In <span className="text-primary"> Touch</span></h2>
